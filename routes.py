e5eb802050b90d0c0f2bb5a01aecdf960404c680
Aleksi <aleksi.saxlund@gmail.com>
2024-04-01 18:19:00 +0300

    Implemented the ability to register and login

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, redirect, request, session
 from random import choice
+import users
 route = Blueprint("routes", __name__)
 
 hardcoded_decks = [(choice(["😳","💀","🐸","😃","🐛","👨‍👩‍👧‍👦","🎞","🚲","🍠","🥩"]), f"Placeholder {i}", i) for i in range(50)]
@@ -8,15 +9,47 @@ hardocded_reviews = [{"username": "asd", "rating": "12", "comment": "hyva on"}, 
 
 @route.route("/")
 def index():
-    return render_template("index.html", logged_in=False, decks=hardcoded_decks, categories=categories)
+    try:
+        log = session["username"]
+    except:
+        log = False
+    return render_template("index.html", logged_in=log, decks=hardcoded_decks, categories=categories)
 
 @route.route("/login")
 def login():
     return render_template("login.html")
 
-@route.route("/register")
+@route.route("/login/check", methods=["POST"])
+def login_check():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if not users.login(username, password):
+        return redirect("/login")
+
+    session["username"] = username
+    return redirect("/")
+
+@route.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@route.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+
+@route.route("/register/check", methods=["POST"])
+def register_check():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_check = request.form["password_check"]
+
+    try:
+        users.register(username, password, password_check)
+        return redirect("/login")
+    except:
+        return redirect("/register")
 
 @route.route("/deck/<int:id>")
 def deck(id):
--- users.py
+++ users.py
@@ -1,8 +1,52 @@
+import os
 from db import db
 from sqlalchemy.sql import text
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
 
-def get_all_users():
-    sql = text("SELECT * FROM users")
-    result = db.session.execute(sql)
-    users = result.fetchall()
-    return users
+class UserInputError(Exception):
+    pass
+
+
+def login(username, password):
+    sql = text("SELECT password FROM users WHERE username = :username")
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
+    if user == None:
+        return False
+    if not check_password_hash(user[0], password):
+        return False
+    
+    session["username"] = username
+    session["csrf_token"] = os.urandom(16).hex()
+    return True
+
+def logout():
+    del session["username"]
+    del session["csrf_token"]
+
+def register(username, password, password_check):
+
+    if validator(username, password, password_check):
+        hash_value = generate_password_hash(password)
+        sql = text("INSERT INTO users (username, password) VALUES (:username, :password)")
+        db.session.execute(sql, {"username": username, "password": hash_value})
+        db.session.commit()
+
+        return True
+    return False
+
+def validator(username, password, password_check):
+    if not username or not password or not password_check:
+        raise UserInputError("Username and password are required")
+    if len(username) < 4:
+        raise UserInputError("Username must be atleast 3 characters long")
+    if password != password_check:
+        raise UserInputError("Passwords are not the same")
+    if len(password) < 7:
+        raise UserInputError("Password usmt be atleast 5 characters long")
+    if not any(char.isdigit() for char in password):
+        raise UserInputError("Password must contain atleast 1 number")
+    if not any(char in "!@#$%^&*()-_=+[]{};:'\"<>,.?/" for char in password):
+        raise UserInputError("Password must contain atleast 1 special character")
+    return True
